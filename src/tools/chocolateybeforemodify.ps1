35b519008b9d0f88c938e3ef3b209dcf23c4b93f
Jordan Cannon <LiteracyFanatic@gmail.com>
2019-01-30 08:50:18 -0600

    Add modification and uninstallation scripts

---
+++ src/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,1 @@
+﻿Get-Process NiftyWindows -ErrorAction SilentlyContinue | Stop-Process
---
+++ src/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$startMenuPrograms = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs"
+
+$shortcuts = @(
+    "$startMenuPrograms\NiftyWindows.lnk"
+    "$startMenuPrograms\Startup\NiftyWindows.lnk"
+)
+
+foreach ($shortcut in $shortcuts) {
+    if (Test-Path $shortcut) {
+        Remove-Item $shortcut
+    }
+}
